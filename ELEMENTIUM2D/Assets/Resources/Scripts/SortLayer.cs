bc0029de0679d641fac9f0c36c37ef6b4b4a2a12
João Reis <jype_13@hotmail.com>
2015-03-23 19:48:56 +0000

    Added some tiles and sorted layers between sprites - still buggy

--- ELEMENTIUM2D/Assets/Resources/Scripts/SortLayer.cs
+++ ELEMENTIUM2D/Assets/Resources/Scripts/SortLayer.cs
@@ -3,15 +3,20 @@ using System.Collections;
 
 public class SortLayer : MonoBehaviour {
 
+    public enum Objects { STATIC, DYNAMIC };
     private GameObject player;
+    public Objects type; 
 
 	// Use this for initialization
 	void Start () {
-        player = GameObject.FindGameObjectWithTag("Player");
-        GetComponent<Renderer>().sortingOrder = Mathf.RoundToInt(-transform.position.z*10);
+        GetComponent<Renderer>().sortingOrder = Mathf.RoundToInt(-(transform.position.z + 250)*100);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	}
+	    if(type != Objects.STATIC)
+        {
+            GetComponent<Renderer>().sortingOrder = Mathf.RoundToInt(-(transform.position.z + 250) * 100);
+        }
+    }
 }
