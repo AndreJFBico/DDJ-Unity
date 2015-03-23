d39ef55aea2a44886b2712d5e326ed654ec50425
João Reis <jype_13@hotmail.com>
2015-03-23 22:07:32 +0000

    fixed some layer ordering issues

--- ELEMENTIUM2D/Assets/Resources/Scripts/SortLayer.cs
+++ ELEMENTIUM2D/Assets/Resources/Scripts/SortLayer.cs
@@ -1,15 +1,30 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class SortLayer : MonoBehaviour {
 
     public enum Objects { STATIC, DYNAMIC };
+    public enum Position { FRONT, BACK };
     private GameObject player;
-    public Objects type; 
+    public Objects type;
+    public Position position;
+    public List<SpriteRenderer> list;
 
 	// Use this for initialization
 	void Start () {
         GetComponent<Renderer>().sortingOrder = Mathf.RoundToInt(-(transform.position.z + 250)*100);
+        if (list != null && list.Capacity > 0)
+        {
+            foreach (SpriteRenderer sr in list)
+            {
+                if (position == Position.FRONT)
+                    sr.sortingOrder = GetComponent<Renderer>().sortingOrder + 1;
+
+                if (position == Position.BACK)
+                    sr.sortingOrder = GetComponent<Renderer>().sortingOrder - 1;
+            }
+        }
 	}
 	
 	// Update is called once per frame
@@ -17,6 +32,17 @@ public class SortLayer : MonoBehaviour {
 	    if(type != Objects.STATIC)
         {
             GetComponent<Renderer>().sortingOrder = Mathf.RoundToInt(-(transform.position.z + 250) * 100);
+            if (list != null && list.Capacity > 0)
+            {
+                foreach (SpriteRenderer sr in list)
+                {
+                    if (position == Position.FRONT)
+                        sr.sortingOrder = GetComponent<Renderer>().sortingOrder + 1;
+
+                    if (position == Position.BACK)
+                        sr.sortingOrder = GetComponent<Renderer>().sortingOrder - 1;
+                }
+            }
         }
     }
 }
