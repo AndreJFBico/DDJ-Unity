31c5e61fdfbc87fca3a4be1effb422cf9cee7237
ScientX <tori789@gmail.com>
2015-03-30 23:57:20 +0100

    merged

---
+++ ELEMENTIUM2D/Assets/Resources/Scripts/Projectiles/EarthStun.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using Includes;
+
+public class EarthStun : AbilityBehaviour {
+
+    protected override void Awake()
+    {
+        explosion = Resources.Load("Prefabs/Explosions/earthExplosion") as GameObject;
+    }
+
+    protected override void Start()
+    {
+        base.Start();
+        //This has to change one thing is the damage the enemy does when it touches the player the other is the ranged projectile damage
+        damage = EnemyStats.Earth.damage;
+        type = Elements.EARTH;
+    }
+
+    public override void OnCollisionEnter(Collision collision)
+    {
+
+    }
+
+    public void OnTriggerEnter(Collider collider)
+    {
+        if (collider.transform.tag.CompareTo("Enemy") == 0)
+        {
+            if(collider.gameObject.GetComponent<StunnedStatusEffect>() == null)
+            {
+                StunnedStatusEffect sse = collider.gameObject.AddComponent<StunnedStatusEffect>();
+                sse.setDuration(2.0f);
+                sse.applyStatusEffect(collider.gameObject.GetComponent<EnemyScript>());
+            }             
+        }
+    }
+
+    public override void initiate(GameObject startingObject)
+    {
+        transform.parent = startingObject.transform;
+    }
+}
---
+++ ELEMENTIUM2D/Assets/Resources/Scripts/StoreUiElement.cs
@@ -0,0 +1,103 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class StoreUiElement
+{
+    public string name = "";
+    public int maxHorizontalItems = 0;
+    [SerializeField]
+    private int IncPointer = 0;
+    [SerializeField]
+    private int DecPointer = 0;
+    [SerializeField]
+    private int numChecked = 1;
+    [SerializeField]
+    private List<Image> boxes = new List<Image>();
+    [SerializeField]
+    private Text points;
+
+    [SerializeField]
+    private Sprite notChecked;
+    [SerializeField]
+    private Sprite Checked;
+
+    public void setPoints(Text p)
+    {
+        points = p;
+        updateText();    
+    }
+
+    public void loadCheckedSprites()
+    {
+        notChecked = Resources.Load<Sprite>("Sprites/UI/itemNonChecked");
+        Checked = Resources.Load<Sprite>("Sprites/UI/itemChecked");
+    }
+
+    public void addBox(Image box)
+    {
+        boxes.Add(box);
+    }
+    
+    public bool incrementPointer()
+    {
+        checkBox(boxes[IncPointer], true);
+        DecPointer = IncPointer;
+            
+        if (IncPointer < boxes.Count - 1)
+        {
+            IncPointer++;        
+        }
+
+        if (numChecked < boxes.Count)
+        {
+            numChecked++;
+            return true;
+        }
+        return false;
+    }
+
+    public bool decrementPointer()
+    {
+        checkBox(boxes[DecPointer], false);
+        IncPointer = DecPointer;
+
+            
+        if (DecPointer > 0)
+        {
+            DecPointer--;                   
+        }
+
+        if (numChecked > 0)
+        {
+            numChecked--;
+            return true;
+        }
+        return false;
+    }
+
+    private void checkBox(Image box, bool val)
+    {
+        if(val)
+        {
+            box.sprite = Checked;
+        }
+        else
+        {
+            box.sprite = notChecked;
+        }
+    }
+
+    public void updateText()
+    {
+        points.text = numChecked + "/" + maxHorizontalItems; 
+    }
+
+    public void reset()
+    {
+        boxes.Clear();
+        boxes = new List<Image>();
+    }
+}
